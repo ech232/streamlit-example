a82361322f920d8204250161bcc4d74e83eb39c8
Emily Hahn <142950983+ech232@users.noreply.github.com>
2024-03-04 09:08:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,17 +18,29 @@ filtered_data = final_qtables[(final_qtables['hour'] == hour) &
                               (final_qtables['day'] == day) & 
                               (final_qtables['weather'] == weather)]
 
-# Map plotting with pydeck
+# Normalize the Q values
+final_qtables['Q'] /= final_qtables['Q'].max()
+final_qtables['Q'] *= 10 
+
+# Plot lines for routes
 layer = pdk.Layer(
     "LineLayer",
     filtered_data,
     get_source_position=["Start_Longitude", "Start_Latitude"],
     get_target_position=["End_Longitude", "End_Latitude"],
-    get_color="[0, 255, 0, 160]",  # Green lines
+    get_color="[20, 255, 20, 160]",  # Green lines
     get_width="Q",  # Use the Q column for line thickness
     pickable=True,
     auto_highlight=True,
 )
+
+# Plot "nodes" for each location
+locations_data = pd.concat([
+    filtered_data[['Source', 'Start_Latitude', 'Start_Longitude']],
+    filtered_data[['Destination', 'End_Latitude', 'End_Longitude']].rename(columns={
+        'Destination': 'Source', 'End_Latitude': 'Start_Latitude', 'End_Longitude': 'Start_Longitude'
+    })
+]).drop_duplicates().reset_index(drop=True)
 
 # Set the view for Boston
 view_state = pdk.ViewState(
@@ -38,14 +50,14 @@ view_state = pdk.ViewState(
     pitch=0,
 )
 
-# Render the map with the routes
+# Render the map with the routes and location dots
 st.pydeck_chart(pdk.Deck(
-    layers=[layer],
+    layers=[line_layer, location_layer],
     initial_view_state=view_state,
     map_style="mapbox://styles/mapbox/light-v9",  # Light mode map
     tooltip={
-        "html": "<b>Source:</b> {Source}<br>" \
-                "<b>Destination:</b> {Destination}<br>" \
+        "html": "<b>Source:</b> {Source}<br>"
+                "<b>Destination:</b> {Destination}<br>"
                 "<b>Q-Value:</b> {Q}"
     }
 ))
